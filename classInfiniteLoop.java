a2999f0572b0e120da7ededd01a7437123cc49a7
Monish S <programminggeekishere@gmail.com>
2018-12-31 16:31:57 +0530

    Update

--- classInfiniteLoop.java
+++ classInfiniteLoop.java
@@ -1,6 +1,6 @@
 class classInfiniteLoop{
     public static void main(String args[]){
-        for (int i = (int)(Math.pow(2, 31) - 1); i != 1; i++) // try i == 1
+        for (byte i = 127; i > -129; i++) // try i == 1
             System.out.println(i);
     }
 }
